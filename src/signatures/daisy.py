be412d9b12c8581d536728eb82eb8a7d79545104
flavio <flavio86@ufpi.edu.br>
2019-09-28 20:59:04 -0300

    daisy working

--- src/signatures/daisy.py
+++ src/signatures/daisy.py
@@ -4,9 +4,12 @@ Computes Daisy features
 '''
 import numpy as np
 from skimage.feature import daisy
+from skimage.transform import resize
 
 def daisy_features(image, name, label, step_ = 4, rings_=3, histograms_=2, orientations_=8):
     
+    if(image.shape[0] < 32 or image.shape[1] < 32): 
+        image = resize(image, (32, 32))
     a = daisy(image,step = step_, rings = rings_, histograms = histograms_, orientations=orientations_)
     result = a.reshape(-1)
     result = np.asarray(result)
